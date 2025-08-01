b9a94b69e8fea275fb5f34e7e12c416aab1704f0
Panda <shantanu24781089@gmail.com>
2025-08-02 01:52:19 +0530

    Update vite.config.ts

--- backend/frontend/vite.config.ts
+++ backend/frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+  build: {
+    chunkSizeWarningLimit: 1500 // Increase default 500KB limit to 1500KB
+  }
+});
